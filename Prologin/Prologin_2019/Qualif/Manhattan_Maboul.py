58491f1d0cdc17551f702462f8f1b5391fed946e
Wappa <aurelien.isak@epita.fr>
2020-06-12 23:45:40 +0200

    WTF max

--- Prologin/Prologin_2019/Qualif/Manhattan_Maboul.py
+++ Prologin/Prologin_2019/Qualif/Manhattan_Maboul.py
@@ -8,12 +8,13 @@ def manhattan_maboul0(m, jours, n):
     maxi = max(jours)+1
     i=min(jours)
     w = mini
-    for i in range(i,maxi,1):
+    for i in range(0,maxi,1):
         NbAmi =0
         if i in jours:
             j = jours.count(i)
             NbAmi += j
         calend.append(NbAmi)
+    print(calend)
     h=i
     for h in range (mini,maxi):
         fri = 0
@@ -23,10 +24,18 @@ def manhattan_maboul0(m, jours, n):
         rtn.append(fri)
         a += 1
         w += 1- m
-
-
     print(max(rtn))
 
-(n, m) = list(map(int, input().split()))
-jours = list(map(int, input().split()))
-manhattan_maboul0(m, jours, n)
+def maxin (liste):
+    mx =0
+    for i in range (0,len(liste),1):
+        if liste[i] > liste[i-1]:
+            mx = liste[i]
+    print(mx)
+
+maxin([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3])
+
+
+#(n, m) = list(map(int, input().split()))
+#jours = list(map(int, input().split()))
+#manhattan_maboul0(m, jours, n)
