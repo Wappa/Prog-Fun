47937ae614ed4bfdc97030c6c75c5a4fe6ca297b
Wappa <aurelien.isak@epita.fr>
2020-06-13 16:19:48 +0200

    Normalement fini Manhattan

--- Prologin/Prologin_2019/Qualif/Manhattan_Maboul.py
+++ Prologin/Prologin_2019/Qualif/Manhattan_Maboul.py
@@ -8,22 +8,27 @@ def manhattan_maboul0(m, jours, n):
     maxi = max(jours)+1
     i=min(jours)
     w = mini
-    for i in range(0,maxi,1):
-        NbAmi =0
-        if i in jours:
-            j = jours.count(i)
-            NbAmi += j
-        calend.append(NbAmi)
-    h=i
-    for h in range (mini,maxi):
-        fri = 0
-        for w in range(w,a+1,1):
-            if w < len(calend):
-                fri += calend[w]
-        rtn.append(fri)
-        a += 1
-        w += 1- m
-    print(max(rtn))
+    if mini == maxi-1:
+        rtn.append(jours.count(mini))
+        print(max(rtn))
+    else :
+
+        for i in range(0,maxi,1):
+            NbAmi =0
+            if i in jours:
+                j = jours.count(i)
+                NbAmi += j
+            calend.append(NbAmi)
+        h=i
+        for h in range (mini,maxi):
+            fri = 0
+            for w in range(w,a+1,1):
+                if w < len(calend):
+                    fri += calend[w]
+            rtn.append(fri)
+            a += 1
+            w += 1- m
+        print(max(rtn))
 
 
 (n, m) = list(map(int, input().split()))
