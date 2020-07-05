3a7025ae6388765e18835a5bda1fc4feb277b977
Wappa <aurelien.isak@epita.fr>
2020-07-05 21:20:11 +0200

    Add all interesting work from linked list

---
+++ DataStructure/LinkedList/Delete.py
@@ -0,0 +1,65 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the deleteNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def deleteNode(head, position):
+    count = 0
+    current = head
+    if position == 0:
+        head = current.next
+        current = None
+    else:
+        while count != (position-1):
+            count = count + 1
+            current = current.next
+        temp = current.next
+        current.next = temp.next
+        temp = None
+    return head
+
+
+if __name__ == '__main__':
